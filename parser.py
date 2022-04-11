be6d6979a55504c28bd3fd0d09ed1c29985b6a96
homePoliceMemos <policememos@gmail.com>
2022-04-11 21:35:25 +0500

    final version done!

--- parser.py
+++ parser.py
@@ -1,29 +1,30 @@
-
 # [31/Jul/2020 22:30:21] POP3: User rkorolev@jlr-autoplus.ru authenticated  from IP address 192.168.16.15
 
 
+
+        
+
+def runner(filename, keyword):
+    with open(filename, encoding='utf-8') as file:
+        result = list()
+        for line in file.readlines():
+            line = line.lower().split()
+            if searcher(line, keyword):
+                str = ' '.join(line)
+                str += ' \n'
+                result.append(str)
+        
+        
+        return result
+            
 def searcher(list,kword):
-    if kword in list:
+    user = list[4].split('@')[0]
+    if user in kword:
         return list
     else:
         return 0
 
-        
-
-
-with open('text.txt', encoding='utf-8') as file:
-    result = list()
-    for line in file.readlines():
-        line = line.split()
-        if searcher(line, 'nedopekina@jlr-autoplus.ru'):
-            str = ' '.join(line)
-            str += ' \n'
-            result.append(str)
-    
-    
-    print(*result)
-        
-
-
+a = runner('text.txt', 'nedopekina@jlr-autoplus.ru')
+print(*a)
 
     
